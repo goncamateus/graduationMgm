624643fce1b2fc7d8b4c0e1abf04519d4d246787
goncamateus <mgm4@cin.ufpe.br>
2019-10-07 21:06:38 -0300

    Updates shell script

--- shells/run_agent.sh
+++ shells/run_agent.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-HFO_mgm/bin/HFO --fullstate --no-logging --headless --defense-agents=3 --offense-npcs=3 --defense-npcs=1 --offense-team=helios --trials $1 &
+HFO_mgm/bin/HFO --fullstate --no-logging --headless --defense-agents=7 --offense-npcs=7 --defense-npcs=1 --offense-team=helios --trials $1 &
 sleep 5
 # Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
+python ./agents/agent.py &
+sleep 1
+python ./agents/agent.py &
+sleep 1
+python ./agents/agent.py &
+sleep 1
+python ./agents/agent.py &
+sleep 1
 python ./agents/agent.py &
 sleep 1
 python ./agents/agent.py &
