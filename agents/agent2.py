ff69d52c30bb024865870c978cc8ab7193e4c772
goncamateus <mgm4@cin.ufpe.br>
2019-10-30 19:21:26 -0300

    Adds another script

---
+++ agents/agent2.py
@@ -0,0 +1,18 @@
+from base_agent import Agent as DQNAgent
+from base_agent_ddpg import DDPGAgent
+from graduationmgm.lib.Neural_Networks.DDPG import DDPG
+from graduationmgm.lib.Neural_Networks.DQN import DQN
+from graduationmgm.lib.Neural_Networks.Dueling_DQN import DDQN
+
+
+def main():
+    agent = DQNAgent(DQN, False, port=9000)
+    # agent = DDPGAgent(DDPG, False)
+    try:
+        agent.run()
+    except:
+        agent.bye()
+
+
+if __name__ == "__main__":
+    main()
---
+++ shells/run_agent2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+HFO_mgm/bin/HFO --port=9000 --fullstate --no-logging --headless --defense-agents=1 --offense-npcs=1 --defense-npcs=1 --offense-team=helios --defense-team=helios --trials $1 &
+sleep 5
+# Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
+python ./agents/agent2.py &
+sleep 1
+# python ./agents/agent.py &
+# sleep 1
+# python ./agents/agent.py &
+# sleep 1
+
+# The magic line
+#   $$ holds the PID for this script
+#   Negation means kill by process group id instead of PID
+trap "kill -TERM -$$" SIGINT
+wait
