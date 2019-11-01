2de448d4e3ba75776ddee71b7182773a41d6d74d
goncamateus <mgm4@cin.ufpe.br>
2019-10-31 22:58:09 -0300

    Adds Robocin's agent

--- HFO_mgm/bin/Teams.py
+++ HFO_mgm/bin/Teams.py
@@ -101,3 +101,24 @@ class Helios(Team):
       launchOpts = '-g'
     print('Launch npc %s-%d' % (self._name, player_num))
     return self.start_npc_proc(launchOpts)
+
+class RoboCIn(Team):
+  def __init__(self, name, baseDir, libDir, binaryName, logDir, record,
+               host='localhost', port=6000):
+    binaryPath = os.path.join(baseDir, binaryName)
+    options = '-t %s -p %i --config_dir %s/config/formations-dt --log_dir %s '\
+              '--player-config %s/config/player.conf'\
+              % (name, port, baseDir, logDir, baseDir)
+    if record:
+      options += ' --record'
+    offenseOrder =  [11,7,8,9,10,6,3,2,4,5]
+    defenseOrder =  [2,3,4,5,6,7,8,11,9,10]
+    super(RoboCIn, self).__init__(name, binaryPath, libDir, options,
+                                  offenseOrder, defenseOrder)
+
+  def launch_npc(self, player_num):
+    launchOpts = None
+    if player_num == 1:
+      launchOpts = '-g'
+    print('Launch npc %s-%d' % (self._name, player_num))
+    return self.start_npc_proc(launchOpts)
--- HFO_mgm/bin/Trainer.py
+++ HFO_mgm/bin/Trainer.py
@@ -101,6 +101,15 @@ class Trainer(object):
                            binaryName='sample_player', logDir=self._logDir,
                            record=self._record, host='localhost',
                            port=self._serverPort)
+    elif requested_team_name == 'robocin':
+      print('Creating team RoboCIN2d')
+      team_name = 'RoboCIn_' + ('left' if play_offense else 'right')
+      team_dir = os.path.join(teams_dir, 'robocin', 'src')
+      lib_dir = None
+      return Teams.RoboCIn(team_name, team_dir, lib_dir,
+                           binaryName='robocin_player', logDir=self._logDir,
+                           record=self._record, host='localhost',
+                           port=self._serverPort)
     else:
       print('Unknown team requested: ' + requested_team_name)
       sys.exit(1)
